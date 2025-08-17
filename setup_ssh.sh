65288d4322246fa955e00e7cde831a71c0255362
Aleshin-a <aleshin23111996@mail.ru>
2025-08-17 22:51:42 +0300

    Create setup_ssh.sh

---
+++ setup_ssh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Установка SSH-сервера
+sudo apt install -y openssh-server
+
+# Создание ключа SSH (рекомендуется, если нет)
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
+# Копирование ключа на сервер (или в authorized_keys, если на локальной машине)
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
+# Установка правильных прав доступа
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
+
+echo "SSH настроен. Теперь можно подключаться без пароля."
